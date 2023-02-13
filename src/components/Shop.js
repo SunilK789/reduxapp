70382488e676d1f2d53fa790d00b0b4bb395cad5
Sunil Kumar <sunilpalarya@gmail.com>
2023-02-13 19:36:54 +0530

    update shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,21 +5,24 @@ import {actionCreators} from "../state/index"
 
 const Shop = () => {
 	const dispatch = useDispatch()
-	const action = bindActionCreators(actionCreators, dispatch)
+	const {depositMoney, withdrawMoney} = bindActionCreators(
+		actionCreators,
+		dispatch
+	)
 
 	return (
 		<div className='conatiner text-center'>
 			<h2>Add Balance 100 Rs.</h2>
 			<button
 				className='btn btn-primary mx-2 my-2'
-				onClick={() => action.withdrawMoney(100)}
+				onClick={() => withdrawMoney(100)}
 			>
 				-
 			</button>
 			Add Balance
 			<button
 				className='btn btn-primary mx-2 my-2'
-				onClick={() => action.depositMoney(100)}
+				onClick={() => depositMoney(100)}
 			>
 				+
 			</button>
